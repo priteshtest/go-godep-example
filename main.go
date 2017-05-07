66114ec42e5b67ae133eef705ad441b21265611e
priteshtest <mailtopdmehta@gmail.com>
2017-05-07 08:56:36 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import "fmt" 
+import "flag"
 import "github.com/Masterminds/semver"
 import "github.com/mitchellh/go-homedir"
-import "github.com/kylelemons/go-gypsy"
+import "github.com/kylelemons/go-gypsy/yaml"
 import "github.com/openshift/origin"
-import "github.com/ant0ine/go-json-rest"
-import "github.com/crowdmob/goamz"
+import "github.com/ant0ine/go-json-rest/rest"
 
 type Options struct {
     Query   string `url:"q"`
@@ -16,8 +16,11 @@ type Options struct {
 
 func main() {
     fmt.Println("Intro to Go!")
+    api := rest.NewApi()
     opt := Options{ "foo", true, 2 }
     v, _ := query.Values(opt)
     fmt.Println(v.Encode()) // will output: "q=foo&all=true&page=2"
+    file = flag.String("file", "config.yaml", "(Simple) YAML file to read")
+    config, err := yaml.ReadFile(*file)
     
 }
