e883d8961853075324e9a03b703dac3498c310bf
priteshmehta <mehta.pritesh@gmail.com>
2017-05-07 09:12:26 +0800

    updated

--- main.go
+++ main.go
@@ -8,19 +8,14 @@ import "github.com/kylelemons/go-gypsy/yaml"
 import "github.com/openshift/origin"
 import "github.com/ant0ine/go-json-rest/rest"
 
-type Options struct {
-    Query   string `url:"q"`
-    ShowAll bool   `url:"all"`
-    Page    int    `url:"page"`
-}
 
 func main() {
     fmt.Println("Intro to Go!")
     api := rest.NewApi()
-    opt := Options{ "foo", true, 2 }
-    v, _ := query.Values(opt)
-    fmt.Println(v.Encode()) // will output: "q=foo&all=true&page=2"
-    file = flag.String("file", "config.yaml", "(Simple) YAML file to read")
+    homedir.Dir() 
+    //fmt.Println(v.Encode()) // will output: "q=foo&all=true&page=2"
+    file := flag.String("file", "config.yaml", "(Simple) YAML file to read")
     config, err := yaml.ReadFile(*file)
+    v, err := semver.NewVersion("1.2.3-beta.1+build345")
     
 }
